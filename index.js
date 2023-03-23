9d841cb4518644b46ee1759f860bfd9899c86c8b
samuel <samuelkelvinmwangi@gmail.com>
2023-03-23 08:22:35 +0300

    finished assingnment

---
+++ index.js
@@ -0,0 +1,63 @@
+let x = a + b
+function add(){
+    return x;
+}
+
+let y = a - b
+function subtract(){
+    return y;
+}
+
+let q = a * b
+function multiply(){
+    return q
+}
+
+let d = a / b
+function divide(){
+    return d
+}
+
+function add(a, b) {
+    return (a + b);
+}
+
+function subtract(a, b){
+    return (a - b);
+}
+
+function multiply(a, b){
+    return (a * b);
+}
+
+function divide(a, b){
+    return (a / b);
+}
+
+function increment(a){
+    return (a + 1);
+}
+
+function decrement(a){
+    return (a - 1);
+}
+
+function makeInt(n) {
+    return n.toString;
+}
+
+function makeInt(n){
+    return parseInt(n);
+}
+
+function makeInt(n){
+    return parseFloat(n);
+}
+
+function preserveDecimal(n){
+    return parseInt(n);
+}
+
+function preserveDecimal(n){
+    return parseFloat(n);
+}
